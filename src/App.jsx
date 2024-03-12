c03b4b310bd4fb5091e0bb58a5b03e360c4e7afa
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-12 22:33:25 +0000

    Import components and assign data as props

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import Join from "./components/Join";
+import Monthly from "./components/Monthly";
+import SignUp from "./components/SignUp";
+import Faq from "./components/Faq";
+
+const data = {
+  title: " Join our community",
+  guarantee: "30-day, hassle-free money back guarantee",
+  titleText:
+    "Gain access to our full library of tutorials along with expert code reviews. \
+Perfect for any developers who are serious about honing their skills.",
+  subscriptionHeading: " Monthly Subscription",
+  price: 29,
+  signupText: "Sign Up",
+  faqTitle: "Why Us",
+  faqReasons: [
+    "Tutorials by industry experts",
+    "Peer & expert code review",
+    "Coding exercises",
+    "Access to our GitHub repos",
+    "Community forum",
+    "Flashcard decks",
+    "New videos every week",
+  ],
+};
+
+function App() {
+  return (
+    <main>
+      <Join
+        title={data.title}
+        guarantee={data.guarantee}
+        fullText={data.titleText}
+      />
+      <Monthly
+        period={data.subscriptionHeading}
+        fee={data.price}
+        valueProposition={data.access}
+      >
+        <SignUp linkText={data.signupText} />
+      </Monthly>
+      <Faq
+        cardTitle={data.faqTitle}
+        reasons={data.faqReasons}
+      />
+    </main>
+  );
+}
+
+export default App;
