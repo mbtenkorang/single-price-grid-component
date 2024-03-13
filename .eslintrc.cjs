0a9733a5662a8829d73ef2a5c631c913de4ab563
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-13 20:07:07 +0000

    Fix eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,6 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "react/no-unknown-property": ["error", { ignore: "css" }],
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
   },
 };
